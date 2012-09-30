bc7c6ee944bc38e04922b807bc8a6d5ade63f10a
neil hudson <neil.j.hudson@gmail.com>
2012-09-30 02:59:52 -0500

    opening directions in google maps now instead

--- src/com/example/im_hungry/MainActivity.java
+++ src/com/example/im_hungry/MainActivity.java
@@ -2,8 +2,20 @@ package com.example.im_hungry;
 
 import java.util.concurrent.ExecutionException;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
 import com.yelp.Yelp;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -18,12 +30,25 @@ import android.content.Context;
 import android.os.AsyncTask;
 import com.yelp.*;
 
+
+
 public class MainActivity extends Activity {
+	protected GeoPoint current_location;
+	//TODO: move yelp auth stuff somewhere better
+	protected final static YelpAuthInfo yelp_auth_info= new YelpAuthInfo("mWYLNzQlV1jHxOWL9BCK3A",//consumerKey
+			"zF5zKAg419WATpzlR4yGY7iVqWM",//consumerSecret
+			"QWkjswtBialCuKtriQKNq4wkWbYeEVhR",//token
+			"rqlzdngBigqKskrmAoXhc5lqKuU"//tokenSecret
+			);
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        LocationListener locationListener = new MyLocationListener();
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 35000, 10, locationListener);
     }
 
     @Override
@@ -32,46 +57,17 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void onFeedMeButtonClick(View v){
-    	
-//    	YelpAuthInfo auth_info= new YelpAuthInfo("mWYLNzQlV1jHxOWL9BCK3A",//consumerKey
-//				"zF5zKAg419WATpzlR4yGY7iVqWM",//consumerSecret
-//				"QWkjswtBialCuKtriQKNq4wkWbYeEVhR",//token
-//				"rqlzdngBigqKskrmAoXhc5lqKuU"//tokenSecret
-//				);
+    public void onOpenGmapsButtonClick(View v){
     	YelpResultsTask yelp_task=new YelpResultsTask();
         yelp_task.execute();
+    	
+    }
+    public void onFeedMeButtonClick(View v){
+    	
+    	YelpResultsGmapsTask yelp_task=new YelpResultsGmapsTask();
+        yelp_task.execute();
         
-//        String alertdialog_message="blahblahblah";
-//        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-// 
-//			// set title
-//			alertDialogBuilder.setTitle("Your Title");
-// 
-//			// set dialog message
-//			alertDialogBuilder
-//				.setMessage(alertdialog_message)
-//				.setCancelable(false)
-//				.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-//					public void onClick(DialogInterface dialog,int id) {
-//						// if this button is clicked, close
-//						// current activity
-//						MainActivity.this.finish();
-//					}
-//				  })
-//				.setNegativeButton("No",new DialogInterface.OnClickListener() {
-//					public void onClick(DialogInterface dialog,int id) {
-//						// if this button is clicked, just close
-//						// the dialog box and do nothing
-//						dialog.cancel();
-//					}
-//				});
-// 
-//				// create alert dialog
-//				AlertDialog alertDialog = alertDialogBuilder.create();
-// 
-//				// show it
-//				alertDialog.show();
+
     }
     
     public void setYelpResultTextView(String text){
@@ -100,19 +96,22 @@ public class MainActivity extends Activity {
 	public class YelpResultsTask extends AsyncTask<Void, Integer, String> {
 		@Override
 		protected String doInBackground(Void... junk) {
-			//TODO: move yelp auth stuff somewhere better
-			YelpAuthInfo auth_info= new YelpAuthInfo("mWYLNzQlV1jHxOWL9BCK3A",//consumerKey
-					"zF5zKAg419WATpzlR4yGY7iVqWM",//consumerSecret
-					"QWkjswtBialCuKtriQKNq4wkWbYeEVhR",//token
-					"rqlzdngBigqKskrmAoXhc5lqKuU"//tokenSecret
-					);
-	        Yelp yelp = new Yelp(	auth_info.consumerKey(), 
-	        						auth_info.consumerSecret(), 
-	        						auth_info.token(), 
-	        						auth_info.tokenSecret()
+
+			
+	        Yelp yelp = new Yelp(	yelp_auth_info.consumerKey(), 
+	        						yelp_auth_info.consumerSecret(), 
+	        						yelp_auth_info.token(), 
+	        						yelp_auth_info.tokenSecret()
 	        						);
 	        try{
-	        	String response = yelp.search("burritos", 30.361471, -87.164326);
+
+//	        	return current_location.toString();
+
+	        	
+//	        	return current_location.getLatitudeE6());
+	        	String response = yelp.search("fast food", 
+	        			current_location.getLatitudeE6(), 
+	        			current_location.getLongitudeE6());
 	        	if(response!=null && response.length()>0){
 	        		return response;
 	        	}
@@ -130,10 +129,80 @@ public class MainActivity extends Activity {
 			if(result != null && result.length()>0){
 				MainActivity.this.setYelpResultTextView(result);
 //						"got something from yelp");
-				MainActivity.this.openMapRestaurant();
+//				MainActivity.this.openMapRestaurant();
 			}else{
 				MainActivity.this.setYelpResultTextView("got NOTHING");
 			}
 		}
 	}
+	
+	public class YelpResultsGmapsTask extends AsyncTask<Void, Integer, String> {
+		@Override
+		protected String doInBackground(Void... junk) {
+
+	        Yelp yelp = new Yelp(	yelp_auth_info.consumerKey(), 
+	        						yelp_auth_info.consumerSecret(), 
+	        						yelp_auth_info.token(), 
+	        						yelp_auth_info.tokenSecret()
+	        						);
+	        try{
+//	        	30.361471, -87.164326);
+	        	
+	        	String response = yelp.search("fast food", 
+	        			current_location.getLatitudeE6(),
+	        			current_location.getLongitudeE6());
+	        	if(response!=null && response.length()>0){
+	        		return response;
+	        	}
+	        } catch (IllegalStateException e){
+	        	//maybe do something here...
+	        }
+
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			if(result != null && result.length()>0){
+				try {
+					//TODO this json parsing is horrible and will probably cause exceptions when yelp changes its stuff 
+					JSONObject result_as_json= new JSONObject(result);
+					JSONArray directions_lines= ((JSONArray)((JSONObject)((JSONObject)((JSONArray)result_as_json.get("businesses")).get(0)).get("location")).get("display_address"));
+					String direction_string="";
+					for( int i=0;i<directions_lines.length();i++) {
+						direction_string+=" "+directions_lines.getString(i);
+					}
+					MainActivity.this.setYelpResultTextView( direction_string);
+										
+
+	//				String url = "http://maps.google.com/maps?saddr=some+address&daddr=another+Address"
+					String url = "http://maps.google.com/maps?daddr="+direction_string;
+					Intent intent = new Intent(android.content.Intent.ACTION_VIEW,  Uri.parse(url));
+//					MainActivity.this.setYelpResultTextView(result);
+					startActivity(intent);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}else{
+				MainActivity.this.setYelpResultTextView("got NOTHING");
+			}
+		}
+	}
+		
+	private final class MyLocationListener implements LocationListener{
+		  public void onLocationChanged(Location location) {
+		    // Called when a new location is found by the network location provider.
+			  current_location= new GeoPoint((int)location.getLatitude(),(int)location.getLongitude());  
+		    
+		  }
+		  public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+		  public void onProviderEnabled(String provider) {}
+
+		  public void onProviderDisabled(String provider) {}
+	}
 }
+
